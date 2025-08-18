60aa31b3fab40a90c9d2a569e08a9a4615398bb0
Adel B <benchemam.adel@gmail.com>
2025-08-18 13:57:52 -0400

    removed the brackets around the ingredients

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 let recipes = [];
 
-    fetch("recipes/recipes.json")
-      .then((res) => res.json())
-      .then((data) => {
-        recipes = data;
-        console.log("Loaded recipes:", recipes.length);
-      });
+fetch("recipes/recipes.json")
+  .then((res) => res.json())
+  .then((data) => {
+    recipes = data;
+    console.log("Loaded recipes:", recipes.length);
+  });
 
-document.querySelector(".searchbar").addEventListener("submit", function (event) {
+document
+  .querySelector(".searchbar")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     const searchTerm = document.querySelector(".search-input").value;
     let results;
 
     if (searchTerm) {
-        results = recipes.filter(r => r.Title.toLowerCase().includes(searchTerm.toLowerCase()));
-        console.log(results);
+      results = recipes.filter((r) =>
+        r.Title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      console.log(results);
     }
 
     const cardHolder = document.querySelector(".result-holder");
     cardHolder.innerHTML = "";
-    results.forEach(r => {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
+    results.forEach((r) => {
+      let ingredientsList = "<ul>";
+
+      if (Array.isArray(r.Ingredients)) {
+        r.Ingredients.forEach((i) => {
+          ingredientsList += `<li>${i.replace(/^\[|'|\]$/g, "").trim()}</li>`;
+        });
+      } else if (typeof r.Ingredients === "string") {
+        r.Ingredients.split(",").forEach((i) => {
+          ingredientsList += `<li>${i.replace(/^\[|'|\]$/g, "").trim()}</li>`;
+        });
+      }
+
+      ingredientsList += "</ul>";
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
     <div class="container">
       <h4><b>${r.Title}</b></h4>
-      <p><b>Ingredients:</b> ${r.Ingredients}</p>
+      <b>Ingredients:</b> ${ingredientsList}
       <p><b>Instructions:</b> ${r.Instructions}</p>
     </div>
   `;
-  cardHolder.appendChild(card);
-});
-
+      cardHolder.appendChild(card);
+    });
   });
